63e4606406a335b61bb23edc273c0aea9989ac62
ომარი ამაღლობელი <omo.amaglobeli200027@gmail.com>
2025-02-20 08:32:26 -0800

    Initial Commit

---
+++ FirstProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstProject
+//
+//  Created by gstore on 20.02.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
